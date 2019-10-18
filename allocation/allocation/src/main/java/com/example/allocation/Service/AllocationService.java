96a52b96fdd2616f0894f8e76ca25ffe27e72dac
VIVEKPATHI24 <56553813+VIVEKPATHI24@users.noreply.github.com>
2019-10-18 13:59:18 +0530

    Update AllocationService.java

--- allocation/allocation/src/main/java/com/example/allocation/Service/AllocationService.java
+++ allocation/allocation/src/main/java/com/example/allocation/Service/AllocationService.java
@@ -2,11 +2,12 @@ package com.example.allocation.Service;
 
 import com.example.allocation.Order.OrderDto;
 import com.example.allocation.Order.OrderLineDto;
+import com.example.allocation.Order.OrderLineStatusDto;
+import com.example.allocation.Order.OrderStatusDto;
 import com.example.allocation.Release.ReleaseLine;
 import com.example.allocation.Release.ReleasePojo;
 import com.example.allocation.Repository.AllocationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -27,16 +28,18 @@ public class AllocationService implements AllocationInterface {
     @Autowired
     RestTemplate restTemplate;
 
+    int loc=1;
     @Override
-    public List<ReleasePojo> save(OrderDto order) {
+    // @RequestMapping(value = "/allocation/v1/api/")
+    public List<OrderDto> save(OrderDto order) {
         // OrderDto orderDto1 = new OrderDto();
         //   List<ReleasePojo> releasePojos = new ArrayList<>();
         Set<OrderLineDto> orderLineDtos = new HashSet<>();
 
-        for (OrderLineDto orderLineDto : order.getOrderLines()) {
+        for (OrderLineDto orderLineDto : order.getOrderLine()) {
             String aaa = orderLineDto.getItemId();
             ResponseEntity<String> status = restTemplate.getForEntity("http://192.168.1.234:8084/api/v1/item/itemstatus/" + aaa, String.class);
-            System.out.println(status.getBody());
+            //  System.out.println(status.getBody());
 
             if (status.getBody().equals("Active")) {
                 // String loc = orderLineDto.getItemId();
@@ -51,18 +54,18 @@ public class AllocationService implements AllocationInterface {
                         HttpMethod.GET,
                         null, String.class);
 
-                Random rand = new Random();
-                 String randomlocation = location.getBody();
+                // Random rand = new Random();
+//                String randomlocation = location.getBody();
 //              list.get(rand.nextInt(list.size()));
 
 //                System.out.println(randomlocation);
-//                System.out.println("qwertyuiop");
 
                 ReleasePojo releasePojo = new ReleasePojo();
 
                 releasePojo.setOrderId(order.getOrderId());
-                releasePojo.setShipFromLocationId(randomlocation);
-                releasePojo.setReleaseId(order.getOrderId() + rand.nextInt(10));
+                releasePojo.setShipFromLocationId(location.getBody());
+                System.out.println("locationId - " + location.getBody());
+                releasePojo.setReleaseId(order.getOrderId()+(loc+1));
                 releasePojo.setStatus("Allocated");
 
                 ReleaseLine releaseLine = new ReleaseLine();
@@ -75,28 +78,47 @@ public class AllocationService implements AllocationInterface {
                 List<ReleaseLine> releaseLines = new ArrayList<>();
                 releaseLines.add(releaseLine);
                 releasePojo.setReleaseLine(releaseLines);
-
                 //   releasePojos.add(releasePojo);
 
+
+
+
                 HttpEntity<ReleasePojo> updaterelease = new HttpEntity<>(releasePojo);
-                ResponseEntity<ReleasePojo> releaseresponse = restTemplate.exchange("http://192.168.43.69:8085/wearehouse/api/v1/post/", HttpMethod.POST, updaterelease, ReleasePojo.class);
+                ResponseEntity<ReleasePojo> releaseresponse = restTemplate.exchange("http://192.168.1.252:8085/warehouse/api/v1/create/", HttpMethod.POST, updaterelease, ReleasePojo.class);
                 // System.out.println(releaseresponse.getBody());
+/*
+                OrderStatusDto orderStatusDto = new OrderStatusDto();
+                orderStatusDto.setOrderStatus("ALLOCATED");
+                orderStatusDto.setOrderId(order.getOrderId());
+                OrderLineStatusDto orderlinenew = new OrderLineStatusDto();
+                orderlinenew.setItemId(orderLineDto.getItemId());
 
-                OrderDto orderDto = new OrderDto();
-                orderDto.setOrderStatus("Confirmed");
+                orderlinenew.setFulfillmentQuantity(orderLineDto.getQuantity());
+                orderlinenew.setOrderLineId(orderLineDto.getOrderLineId());
+                orderlinenew.setOrderStatus("ALLOCATED");
+                List<OrderLineStatusDto> orderLineStatusDtoList = new ArrayList<>();
+                orderLineStatusDtoList.add(orderlinenew);
+                orderStatusDto.setOrderLine(orderLineStatusDtoList);
 
-                HttpEntity<OrderDto> orderstatus = new HttpEntity<>(orderDto);
-                ResponseEntity<OrderDto> orderstatusresponse = restTemplate.exchange("http://192.168.1.59:8084/order/api/v1/post", HttpMethod.POST, orderstatus, OrderDto.class);
+                HttpEntity<OrderStatusDto> orderstatus = new HttpEntity<>(orderStatusDto);
+                ResponseEntity<OrderDto> orderstatusresponse = restTemplate.exchange("http://192.168.1.59:8084/order/api/v1/orderstatus/update" , HttpMethod.PUT, orderstatus, OrderDto.class);*/
             }
-
-
         }
         return null;
+    }
+
+    @Override
+    public OrderDto put(OrderDto orderstatusDto) {
+
+        //ResponseEntity<OrderDto> orderstatusresponse = restTemplate.exchange("http://192.168.1.59:8084/order/api/v1/orderstatus/update"+ ooo , HttpMethod.PUT, orderstatus, OrderDto.class);
+
+        return orderstatusDto;
     }
 
 
     @Override
     public OrderDto getById(String orderId) {
+
         return null;
     }
 
